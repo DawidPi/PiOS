ba14e0bc27200f074aa1d463d83c3b581848d127
DawidPi <pilarski.dawid94@gmail.com>
2017-03-03 13:26:29 +0100

    gtest automatically downloaded and compiled as dependency

--- CMake/gtest_utils.cmake
+++ CMake/gtest_utils.cmake
@@ -1,10 +1,42 @@
-MACRO(enable_gtest sources targetName)
-    find_package(GTest)
+include(ExternalProject)
+
+MACRO(enable_gtest sources dependencies)
     find_package(Threads)
-    if(GTEST_FOUND)
+    if (PiOS_BUILD_TESTS)
         enable_testing()
-        include_directories(../sources ${GTEST_INCLUDE_DIRS})
+        include_directories(../sources ${GTEST_INCLUDE} ${GMOCK_INCLUDE})
         add_executable(${PROJECT_NAME} ${sources})
-        target_link_libraries(${PROJECT_NAME} Threads::Threads ${GTEST_BOTH_LIBRARIES})
-    endif()
+        target_link_libraries(${PROJECT_NAME} ${dependencies} Threads::Threads gtest gtest_main)
+        add_dependencies(${PROJECT_NAME} ${dependencies} gtest gtest_main)
+    endif ()
 ENDMACRO()
+
+FUNCTION(downloadGtest)
+    ExternalProject_add(googletest
+            GIT_REPOSITORY https://github.com/google/googletest.git
+            GIT_TAG master
+
+            SOURCE_DIR "${CMAKE_BINARY_DIR}/3rdParty/gtest-src"
+            BINARY_DIR "${CMAKE_BINARY_DIR}/3rdParty/gtest-build"
+
+            LOG_DOWNLOAD ON
+            )
+
+    add_library(gtest UNKNOWN IMPORTED)
+    add_library(gtest_main UNKNOWN IMPORTED)
+    add_library(gmock UNKNOWN IMPORTED)
+    add_library(gmock_main UNKNOWN IMPORTED)
+    add_dependencies(gtest googletest)
+    add_dependencies(gtest_main googletest)
+    add_dependencies(gmock googletest)
+    add_dependencies(gmock_main googletest)
+    ExternalProject_Get_Property(googletest binary_dir)
+    set_target_properties(gtest PROPERTIES IMPORTED_LOCATION ${binary_dir}/googlemock/gtest/libgtest.a)
+    set_target_properties(gtest_main PROPERTIES IMPORTED_LOCATION ${binary_dir}/googlemock/gtest/libgtest_main.a)
+    set_target_properties(gmock PROPERTIES IMPORTED_LOCATION ${binary_dir}/googlemock/libgmock.a)
+    set_target_properties(gmock_main PROPERTIES IMPORTED_LOCATION ${binary_dir}/googlemock/libgmock_main.a)
+
+    ExternalProject_Get_Property(googletest source_dir)
+    set(GTEST_INCLUDE ${source_dir}/googletest/include PARENT_SCOPE)
+    set(GMOCK_INCLUDE ${source_dir}/googlemock/include PARENT_SCOPE)
+ENDFUNCTION()
--- CMake/settings.cmake
+++ CMake/settings.cmake
@@ -1,2 +1,3 @@
 set(PiOS_PLATFORM_TYPE "ALL" CACHE STRING "type of platform, on which OS is going to be run on" FORCE)
 set_property(CACHE PiOS_PLATFORM_TYPE PROPERTY STRINGS ARM PC ALL UNKNOWN)
+option(PiOS_BUILD_TESTS "Builds tests using gtest" ON)
